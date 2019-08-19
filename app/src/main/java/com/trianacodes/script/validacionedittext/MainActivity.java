1d1055a8b65146ec5e731a6da811db0294e5f5f4
Worome <41673409+Worome@users.noreply.github.com>
2019-08-19 11:39:10 +0200

    Initial commit

---
+++ app/src/main/java/com/trianacodes/script/validacionedittext/MainActivity.java
@@ -0,0 +1,13 @@
+package com.trianacodes.script.validacionedittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
